f4510f54f0ed08df649ab0c256a9ea7105b4d799
joshuachopra <jchopra@gitlab.com>
2022-02-09 21:12:58 -0500

    register routes to controller funcs

---
+++ routers/book_router.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joshua-chopra/go-crud/controllers"
+)
+
+// BookRouter /*
+// Router grouping together path operation functions
+// for /book/ prefix.
+func BookRouter(rg *gin.RouterGroup) {
+	rg.GET("/", controllers.GetBooks)
+	rg.GET("/:id", controllers.GetBook)
+}
