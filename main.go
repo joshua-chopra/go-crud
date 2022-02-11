688ae381dcfff04eaa4916c7e97dbe8091b44f55
joshuachopra <jchopra@gitlab.com>
2022-02-10 22:30:22 -0500

    add ping to root router instead of /api router.

--- main.go
+++ main.go
@@ -16,18 +16,19 @@ func main() {
 	port := os.Getenv("SERVER_PORT")
 	// instantiate router and add in groups under it.
 	router := gin.Default()
+	// main router will be api, then suffix w/ each resource follows
 	mainRouter := router.Group("/api")
 	{
 		routers.BookRouter(mainRouter.Group("/book"))
 	}
-	// simple testing route.
-	mainRouter.GET("/ping", func(ctx *gin.Context) {
+	// simple testing route for base router. localhost:3000/ping.
+	router.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 	serverPath := fmt.Sprintf("localhost:%s", port)
-	fmt.Println(serverPath)
+	//fmt.Print(serverPath)
 	if err := router.Run(serverPath); err != nil {
 		log.Fatalf("Couldn't start server at port %s, exiting program due to err: \n\t%s\n", port, err)
 	}
