78968b5244cf542d261a878d91c0cb79966375fd
joshuachopra <jchopra@gitlab.com>
2022-02-13 15:30:51 -0500

    move logic for server router to api.go, call in main (refactoring)

--- api/api.go
+++ api/api.go
@@ -1,1 +1,42 @@
 package api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joshua-chopra/go-crud/api/routers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func StartRouter() {
+	router, serverURL := setupRouter()
+	if err := router.Run(serverURL); err != nil {
+		log.Fatalf("Couldn't start server at URL %s, exiting program due to err: \n\t%s\n", serverURL, err)
+	}
+}
+
+func setupRouter() (*gin.Engine, string) {
+	// instantiate initialRouter and add in groups under it.
+	initialRouter := gin.Default()
+	// initially we have our API router, which has
+	// children routers under it
+	apiRouter := initialRouter.Group("/api")
+	{
+		routers.BookRouter(apiRouter.Group("/book"))
+	}
+	// simple testing route for base initialRouter. localhost:3000/ping.
+	initialRouter.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	serverURL := getServerURL()
+	return initialRouter, serverURL
+}
+
+func getServerURL() string {
+	host, port := os.Getenv("SERVER_HOST"),
+		os.Getenv("SERVER_PORT")
+	return fmt.Sprintf("%s:%s", host, port)
+}
--- main.go
+++ main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/joshua-chopra/go-crud/api/routers"
+	"github.com/joshua-chopra/go-crud/api"
 	"github.com/joshua-chopra/go-crud/database"
 	"github.com/joshua-chopra/go-crud/internal"
-	"log"
-	"net/http"
-	"os"
 )
 
 func main() {
 	internal.Setup()
-	database.ConnectDatabase()
-	port := os.Getenv("SERVER_PORT")
-	// instantiate router and add in groups under it.
-	router := gin.Default()
-	// main router will be api, then suffix w/ each resource follows
-	mainRouter := router.Group("/api")
-	{
-		routers.BookRouter(mainRouter.Group("/book"))
-	}
-	// simple testing route for base router. localhost:3000/ping.
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	serverPath := fmt.Sprintf("localhost:%s", port)
-	//fmt.Print(serverPath)
-	if err := router.Run(serverPath); err != nil {
-		log.Fatalf("Couldn't start server at port %s, exiting program due to err: \n\t%s\n", port, err)
-	}
+	database.InitializeDatabase()
+	api.StartRouter()
 }
