d1740d9e1630bb630a9a560fbc1d8e54789e6819
joshuachopra <jchopra@gitlab.com>
2022-02-13 16:18:26 -0500

    ensure root dir is explicitly used for loading env, since godotenv package has limitation depending on where it is run from, i.e., in tests directory it won't go look in the project root.

--- internal/setup.go
+++ internal/setup.go
@@ -1,13 +1,35 @@
 package internal
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
+	"path/filepath"
+	"runtime"
 )
 
+// Setup /*
+/*
+Setup func for our app. Currently
+just loads the env file specified.
+*/
 func Setup() {
-	err := godotenv.Load(".env.local")
+	loadEnv()
+}
+
+/* use the following to ensure that whenever setup is called,
+e.g., from tests directory that we will get the package path,
+go up a directory to the root of
+the project and load the env.
+*/
+func loadEnv() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	fmt.Println(basepath)
+	//err := godotenv.Load(".env.local")
+	err := godotenv.Load(filepath.Join(basepath, "../.env.local"))
 	if err != nil {
-		log.Fatal("Could not load env file. Exiting program.")
+		log.Fatalf("Could not load env file. Exiting program,"+
+			"see error: %v\n", err)
 	}
 }
