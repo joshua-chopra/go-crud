b9e1c5ac8a368a0a8a32d358bd5504c5022cd985
joshuachopra <jchopra@gitlab.com>
2022-02-13 18:26:16 -0500

    test getting the 2 books we seed the database w/ on construction and unmarshal the JSON

--- api/api_test.go
+++ api/api_test.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/joshua-chopra/go-crud/database"
 	"github.com/joshua-chopra/go-crud/internal"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -36,5 +38,17 @@ func TestGetAllBooks(t *testing.T) {
 
 	writer := httptest.NewRecorder()
 	rtr.ServeHTTP(writer, testReq)
+	log.Println(writer.Body.String())
+
+	// response is of the form "data": [ array of book objects ]
+	var resp map[string][]database.Book
+	err = json.Unmarshal([]byte(writer.Body.String()), &resp)
+
+	books := resp["data"]
+	//fmt.Println(books)
+
 	assert.Equal(t, 200, writer.Code)
+	assert.Nil(t, err)
+	// 2 books after initial seeding phase.
+	assert.Equal(t, len(books), 2)
 }
