c7dbc47fa2177229ab30a0fb5bbf5b6405f2533c
joshuachopra <jchopra@gitlab.com>
2022-02-13 22:55:54 -0500

    modify for passing in ID then constructing request (simulating client's request to server) to get the book.

--- testing_helpers/db_helpers.go
+++ testing_helpers/db_helpers.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/joshua-chopra/go-crud/database"
-	"net/http"
 	"net/http/httptest"
 )
 
-func GetBookInDB(rtr *gin.Engine, req *http.Request) (database.Book, error, *httptest.ResponseRecorder) {
+func GetBookInDB(rtr *gin.Engine, id uint) (database.Book, error, *httptest.ResponseRecorder) {
 	SetupProject(true, true)
+	req := GetBookRequest(id)
 	mockResp := ExecuteRequest(rtr, req)
 	var parsedResp map[string]database.Book
 	err := json.Unmarshal([]byte(mockResp.Body.String()), &parsedResp)
